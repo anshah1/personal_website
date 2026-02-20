30bed8476543e26916f86f817536d21d17cbc243
anshah1 <anshshah2025@gmail.com>
2026-02-20 03:39:56 -0500

    somewhat functional

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import About from './components/About';
+//import About from './components/About';
 import Projects from './components/Projects';
-import Experiences from './components/Experiences';
-import Navbar from './components/Navbar';
-import Contact from './components/Contact';
+//import Experiences from './components/Experiences';
+//import Navbar from './components/Navbar';
+//import Contact from './components/Contact';
 
 function App() {
-  return
+  return <Projects />;
 }
 
 export default App;
